c3fae0202d61b3ede0bd53c0c17d98091617ca35
Will Sutherland <wgpsutherland@gmail.com>
2022-12-19 17:18:00 +0000

    add ability to run test input

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
-def get_puzzle_input_as_rows(year: int, question: int):
-    with open(f"{year}/question_{question}_input.txt") as f:
+def get_puzzle_input_as_rows(year: int, question: int, test: bool = False):
+    file_name = f"{year}/question_{question}{'_test' if test else ''}_input.txt"
+    with open(file_name) as f:
         for row in f.readlines():
             yield row.strip()
 
